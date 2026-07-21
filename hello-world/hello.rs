de9d701de44b207f9f0914dcf9dcbf3c4613ed1a
jonahbenedicto <jb@jonahbenedicto.com>
2026-07-21 16:48:12 +1000

    add hello-world

---
+++ hello-world/hello.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, World!");
+}
+// Compile: rustc hello-world/hello.rs
+// Run: ./hello
+// println! is a macro that prints text to the console.
+// ! indicates that println is a macro, not a function.
+// Macros are a way of writing code that writes other code, 
+// which is known as metaprogramming.
